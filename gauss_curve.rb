a2bfe4c6ae55f1ec8f872b2eee2e6047576ddfcd
Levshits <levshits.l@gmail.com>
2014-10-14 10:16:32 +0300

    base is ready

--- gauss_curve.rb
+++ gauss_curve.rb
@@ -1,19 +1,16 @@
-require 'Mathn'
+
 class Gauss_curve
   def initialize(points, posibility)
     sum = 0
     points.each{|element| sum+= element}
     @expectation =  sum/points.size.to_f
     p @expectation
-    sum = 0
     points.each{|element| sum+= (@expectation-element)**2 }
-    p sum
     @sigma = Math.sqrt(sum/points.size)
     p @sigma
     @posibility = posibility
   end
   def get_value(x)
-    p x
-    p Math.exp(-0.5*((x-@expectation)/@sigma)**2)/(@sigma*Math.sqrt(2*Math::PI))*@posibility
+    Math.exp(-0.5*((x-@expectation)/@sigma)**2)/(@sigma*Math.sqrt(2*Math::PI))*@posibility
   end
 end
--- main.rb
+++ main.rb
@@ -1,36 +1,50 @@
 require_relative 'gauss_curve'
-require "gnuplot"
+require 'tk'
+require 'gruff'
 
 # See sin_wave.rb first
-first = []
-(0...10000).each{first<<rand(400)}
-second = []
-(0...10000).each{second<<rand(400)+200}
-first_curve = Gauss_curve.new(first,0.3);
-second_curve = Gauss_curve.new(second,0.7);
-Gnuplot.open do |gp|
-  Gnuplot::Plot.new( gp ) do |plot|
-
-    plot.terminal "png size 400,400"
-    plot.output File.expand_path("../sin_wave.png", __FILE__)
-
-    # see sin_wave.rb
-    plot.xrange "[-5:605]"
-    plot.ylabel "y"
-    plot.xlabel "x"
-    x = (-5..605).collect { |v| v.to_f }
-    y = x.collect { |v| first_curve.get_value(v) }
-    plot.data << Gnuplot::DataSet.new( [x,y] ) do |ds|
-      ds.with = "lines"
-      ds.linewidth = 1
-    end
-    x = (-5..605).collect { |v| v.to_f }
-    y = x.collect { |v| second_curve.get_value(v) }
-    plot.data << Gnuplot::DataSet.new( [x,y] ) do |ds|
-      ds.with = "lines"
-      ds.linewidth = 1
-    end
-
-  end
+def create_source_array(max, shift)
+  result = []
+  (0...50000).each{result<<rand(max)+shift}
+  return result
 end
-puts 'created sin_wave.gif'
+def create_pict(curves)
+  g = Gruff::Line.new
+  g.title = 'Wow!  Look at this!'
+  curves.each_index{|index| g.data "Set ##{index+1}",curves[index]}
+  g.write('exciting.png')
+end
+def calculate_lines(curves, absolute_max)
+  result = []
+  x = (0..1000).collect { |v| v =(v.to_f/1000*absolute_max) }
+  curves.each{|element|
+    result<< x.collect { |v| element.get_value(v) }
+    }
+  return result
+end
+def calculate_error(lines_data, absolute_max)
+  error_of_finding = 0
+  false_nervous = 0
+  x = 0
+  (0...1000).each{|index|
+  if lines_data[0][index]>lines_data[1][index]
+    false_nervous+=lines_data[1][index]
+  else
+    if x ==0
+      x = index
+    end
+    error_of_finding+=lines_data[0][index]
+  end}
+  error_of_finding*=absolute_max.to_f/1000
+  false_nervous*=absolute_max.to_f/1000
+  x = (x*absolute_max.to_f/1000).to_i
+  return [error_of_finding,false_nervous,x]
+end
+first = create_source_array(100,0)
+second = create_source_array(100,50)
+first_curve = Gauss_curve.new(first,0.5);
+second_curve = Gauss_curve.new(second,0.5);
+lines_data = calculate_lines([first_curve,second_curve], 150)
+error = calculate_error(lines_data,150)
+p error
+create_pict(lines_data)
